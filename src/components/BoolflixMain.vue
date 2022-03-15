44e182d331ef200b84d92efa4437cfd92541f647
albertofanini-prog <albertofanini@outlook.com>
2022-03-15 16:28:49 +0100

    Stampa dinamica da API

--- src/components/BoolflixMain.vue
+++ src/components/BoolflixMain.vue
@@ -1,7 +1,23 @@
 <template>
     <main>
-        <input type="text" placeholder="Scrivi qua">
-        <button>Search</button>
+        <input
+        type="text"
+        placeholder="Scrivi qua..."
+        v-model="searchMovie"
+        >
+        <button @click="searchAPI(searchMovie)">
+            Search
+        </button>
+        <div
+        class="singleMovie"
+        v-for="movie in movies"
+        :key="movie.id"
+        >
+            <h3>{{movie.title}}</h3>
+            <h4>{{movie.original_title}}</h4>
+            <h6>{{movie.original_language}}</h6>
+            <h6>{{movie.vote_average}}</h6>
+        </div>
     </main>
 </template>
 
@@ -13,13 +29,34 @@ export default {
     name: 'BoolflixMain',
     data(){
         return{
-            search: 'fight club',
+            searchMovie: '',
             movies:[]
         }
     },
-    created(){
-        axios.get('https://api.themoviedb.org/3/movie/550?api_key=21468023db35f90fc29dfeda12ec6478')
-    }
+    methods: {
+        searchAPI(input){
+            this.searchMovie = input;
+            axios.get(
+                `https://api.themoviedb.org/3/search/movie?api_key=21468023db35f90fc29dfeda12ec6478&language=en-US&query=`
+                +   
+                this.searchMovie
+                +
+                `&page=1&include_adult=false`
+            )
+            .then((response) =>{
+                this.movies = response.data.results;
+            })
+            // ,{
+            //     params:{
+            //         apiKey: '21468023db35f90fc29dfeda12ec6478',
+            //         query: this.searchMovie,
+            //     }
+            // }
+        }
+    },
+    // created(){
+    //     axios.get('https://api.themoviedb.org/3/movie/550?api_key=21468023db35f90fc29dfeda12ec6478')
+    // }
 }
 </script>
 
